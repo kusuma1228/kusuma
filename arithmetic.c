517f54b4c51cb02e79061aa881e9f66a46be060b
kusuma1228 <tumatikusumapriya@gmail.com>
2023-11-09 11:44:54 +0530

    priya

---
+++ arithmetic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10, b = 20, res;
+    res = a + b;
+    printf("a+b = %d\n", res);
+    res = a - b;
+    printf("a-b =%d\n",res);
+    res = a*b;
+    printf("a*b = %d\n",res);
+    res = a/b;
+    printf("a/b = %d\n",res);
+    res = a % b;
+    printf("a%%b =%d\n",res);
+    return 0;
+    
+}
--- float.c
+++ float.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    
+   float a = 9.0f;
+   float b = 2.5f;
+   float c = 2E-4f;
+   printf("%f\n", a);
+   printf("%f\n", b);
+   printf("%f",c);
+   return 0;
+ 
 }
